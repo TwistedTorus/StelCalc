4df3353761f7b2e189d8acb0cd63c063cc56b722
TwistedTorus <62248088+TwistedTorus@users.noreply.github.com>
2021-12-21 01:17:08 +0000

    Add files via upload

--- js_scripts/job_output_model.js
+++ js_scripts/job_output_model.js
@@ -7,7 +7,7 @@ class job {
 		this.job_output_vector = job_output_vector;
 		this.job_upkeep_vector = job_upkeep_vector;
 		this.pop_upkeep_vector = pop_upkeep_vector;
-		this.job_sybmol_url = symbol_url;
+		this.symbol_url = symbol_url;
 
 
 		this.job_vector = [] 
@@ -15,6 +15,14 @@ class job {
 				this.job_vector.push(job_output_vector[i] - job_upkeep_vector[i] - pop_upkeep_vector[i]);
 			}
 	}
+}
+
+class resource {
+	constructor(name, symbol_url){
+		this.name = name;
+		this.symbol_url = symbol_url;
+	}
+
 }
 
 function transpose(matrix) {
@@ -27,6 +35,7 @@ class outputs_model {
 		this.resource_target = [0,0,0,0,0,0];
 		this.jobs_needed = [0,0,0,0,0,0];
 		this.jobs_list = [];
+		this.resources_list = [];
 		console.debug("jobs model initialised");
 	}
 
@@ -34,14 +43,31 @@ class outputs_model {
 		let worker_upkeep = [0,0,1,0,0.25,0];
 		let specialist_upkeep = [0,0,1,0,0.5,0];
 		let default_symbol_url = "https://stellaris.paradoxwikis.com/images/4/4f/Job_primitive_laborer.png";
-		let Technician = new job("Technician", [6,0,0,0,0,0], [0,0,0,0,0,0], worker_upkeep, "https://stellaris.paradoxwikis.com/images/7/74/Job_technician.png");
-		let Miner = new job("Miner", [0,4,0,0,0,0], [0,0,0,0,0,0], worker_upkeep, "https://stellaris.paradoxwikis.com/images/f/fc/Job_miner.png");
-		let Farmer = new job("Farmer", [0,0,6,0,0,0], [0,0,0,0,0,0], worker_upkeep, "https://stellaris.paradoxwikis.com/images/a/a0/Job_farmer.png");
-		let Metallurgist = new job("Metallurgist", [0,0,0,3,0,0], [0,6,0,0,0,0], specialist_upkeep, "https://stellaris.paradoxwikis.com/images/5/5b/Job_foundry.png");
-		let Artisan = new job("Artisan", [0,0,0,0,6,0], [0,6,0,0,0,0], specialist_upkeep, "https://stellaris.paradoxwikis.com/images/5/53/Job_artisan.png");
-		let Researcher = new job("Researcher", [0,0,0,0,0,12], [0,0,0,0,2,0], specialist_upkeep, "https://stellaris.paradoxwikis.com/images/9/94/Job_researcher.png");
+		let Technician = new job("Technician", [6,0,0,0,0,0], [0.5,0,0,0,0,0], worker_upkeep, "https://stellaris.paradoxwikis.com/images/7/74/Job_technician.png");
+		let Miner = new job("Miner", [0,4,0,0,0,0], [0.5,0,0,0,0,0], worker_upkeep, "https://stellaris.paradoxwikis.com/images/f/fc/Job_miner.png");
+		let Farmer = new job("Farmer", [0,0,6,0,0,0], [0.5,0,0,0,0,0], worker_upkeep, "https://stellaris.paradoxwikis.com/images/a/a0/Job_farmer.png");
+		let Metallurgist = new job("Metallurgist", [0,0,0,3,0,0], [1,6,0,0,0,0], specialist_upkeep, "https://stellaris.paradoxwikis.com/images/5/5b/Job_foundry.png");
+		let Artisan = new job("Artisan", [0,0,0,0,6,0], [1,6,0,0,0,0], specialist_upkeep, "https://stellaris.paradoxwikis.com/images/5/53/Job_artisan.png");
+		let Researcher = new job("Researcher", [0,0,0,0,0,12], [1,0,0,0,2,0], specialist_upkeep, "https://stellaris.paradoxwikis.com/images/9/94/Job_researcher.png");
 
 		this.jobs_list = [Technician, Miner, Farmer, Metallurgist, Artisan, Researcher];
+	}
+	
+	add_default_resources() {
+		let Energy_url = "https://stellaris.paradoxwikis.com/images/a/a9/Energy.png";
+		let Min_url = "https://stellaris.paradoxwikis.com/images/1/10/Minerals.png";
+		let Food_url = "https://stellaris.paradoxwikis.com/images/c/c6/Food.png";
+		let Alloys_url = "https://stellaris.paradoxwikis.com/images/9/9a/Alloys.png";
+		let Cg_url = "https://stellaris.paradoxwikis.com/images/f/fe/Consumer_goods.png";
+		let Science_url = "https://stellaris.paradoxwikis.com/images/thumb/2/20/Research.png/24px-Research.png";
+		let Energy = new resource("Energy", Energy_url);
+		let Minerals = new resource("Minerals", Min_url);
+		let Food = new resource("Food", Food_url);
+		let Alloys = new resource("Alloys", Alloys_url);
+		let Consumer_goods = new resource("Consumer goods", Cg_url);
+		let Science = new resource("Science", Science_url);
+		this.resources_list = [Energy, Minerals, Food, Alloys, Consumer_goods, Science];
+
 	}
 
 	set_target( new_target) {
@@ -53,21 +79,17 @@ class outputs_model {
 		for (let job of this.jobs_list) {
 			j_m.push(job.job_vector);
 		}
-		console.debug(j_m);
 		j_m = transpose(j_m);
-		
-		console.debug(j_m);
 
 		let J_Matrix = Matrix.create(...j_m);
-		console.debug(J_Matrix.toString());
 		let jobs_vector = J_Matrix.solve(Vector.create(...this.resource_target));
-		
+
 		for (let i in jobs_vector) {
-			this.jobs_needed[i] = jobs_vector[i].toPrecision(3);
+			this.jobs_needed[i] = jobs_vector[i].toFixed(1);
 		}
 
 	}
 
 }
 
-export {job, outputs_model };
+export {job, resource, outputs_model };
--- js_scripts/page_model.js
+++ js_scripts/page_model.js
@@ -1,65 +1,161 @@
 // JavaScript source code
-import {job, outputs_model} from "./job_output_model.js"
-
-class resource {
-	constructor(name, symbol_url){
-		this.name = name;
-		this.symbol_url = symbol_url;
-	}
-
-}
+import {job, resource, outputs_model} from "./job_output_model.js"
 
 class page_model {
 	constructor(){
 
 	this.om = new outputs_model();
 	this.om.add_default_jobs();
-	this.resource_List = [];
-	this.set_resource_List();
+	this.om.add_default_resources();
 
+	this.create_job_definition_table();
+	this.create_resource_definition_table();
 	this.jobs_table = document.createElement('table');
 	this.jobs_table.setAttribute("id", "jobs_matrix_table");
 	this.set_jobs_table();
 	this.edits_table = document.createElement('table');
 	this.edits_table.setAttribute("id", "edits_table");
+	this.target_table = document.createElement("table");
+	this.create_target_table();
+	this.jobs_needed_table = document.createElement('table');
+	this.create_jobs_needed_table();
 
-	this.boundGet = this.get_target.bind(this); // bound functions that utilize event listeners
+	this.boundCalculate = this.calculate.bind(this); // bound functions that utilize event listeners
 	this.boundEdit = this.edit_jobs_table.bind(this);
 	this.boundApply = this.apply_edit.bind(this);
 
 	}
 
-	set_resource_List() {
-		let Energy_url = "https://stellaris.paradoxwikis.com/images/a/a9/Energy.png";
-		let Min_url = "https://stellaris.paradoxwikis.com/images/1/10/Minerals.png";
-		let Food_url = "https://stellaris.paradoxwikis.com/images/c/c6/Food.png";
-		let Alloys_url = "https://stellaris.paradoxwikis.com/images/9/9a/Alloys.png";
-		let Cg_url = "https://stellaris.paradoxwikis.com/images/f/fe/Consumer_goods.png";
-		let Science_url = "https://stellaris.paradoxwikis.com/images/thumb/2/20/Research.png/24px-Research.png";
-		let Energy = new resource("Energy", Energy_url);
-		let Minerals = new resource("Minerals", Min_url);
-		let Food = new resource("Food", Food_url);
-		let Alloys = new resource("Alloys", Alloys_url);
-		let Consumer_goods = new resource("Consumer goods", Cg_url);
-		let Science = new resource("Science", Science_url);
-		this.resource_List = [Energy, Minerals, Food, Alloys, Consumer_goods, Science];
+	create_job_definition_table() {
+		var c, r, t, h;
+		t = document.createElement("table");
+		t.style = "display: inline-block; padding-right: 20px";
+		h = t.createTHead();		
+		r = t.insertRow();
+		r.style.fontWeight="bold";
+		
+		c = r.insertCell();
+		c.innerHTML = "Job";
+		c = r.insertCell();
+		c.innerHTML = "Symbol";
+
+		for (let job of this.om.jobs_list) {
+			r = t.insertRow();
+			c = r.insertCell();
+			c.innerHTML = job.job_name;
+			c = r.insertCell();
+			c.innerHTML = "<img src= " + job.symbol_url + " alt=\"Resource_Symb\" width=\"20\" height=\"20\"> ";
+		}
+
+		document.body.insertBefore(t, document.getElementById("jobs"))
+
+	}
+
+	create_resource_definition_table() {
+		var c, r, t;
+		t = document.createElement("table");
+		t.style = "display: inline-block";
+		r = t.insertRow();
+		r.style.fontWeight="bold";
+
+		c = r.insertCell();
+		c.innerHTML = "Resource";
+		c = r.insertCell();
+		c.innerHTML = "Symbol";
+		console.debug(this.om.resources_list);
+		for (let resource of this.om.resources_list) {
+			r = t.insertRow();
+			c = r.insertCell();
+			c.innerHTML = resource.name;
+			c = r.insertCell();
+			c.innerHTML = "<img src= " + resource.symbol_url + " alt=\"Resource_Symb\" width=\"20\" height=\"20\"> ";
+		}
+
+		document.body.insertBefore(t, document.getElementById("jobs"))
+
+	}
+
+	create_target_table() {
+		var c, r, t;
+		t = this.target_table;
+		t.width = 400;
+		r = t.insertRow(); 
+		c = r.insertCell();
+		for (let resource of this.om.resources_list) {
+			c = r.insertCell();
+			c.innerHTML = "<img src= " + resource.symbol_url + " alt=\"Resource_Symb\" width=\"20\" height=\"20\"> ";
+		}
+		r = t.insertRow();
+		c = r.insertCell();
+		c.innerHTML = "Resource Targets: "
+		for (let i in this.om.resources_list) {
+			c = r.insertCell();
+				
+			var x = document.createElement("INPUT");
+			x.setAttribute("size", 1);
+			//x.setAttribute("type", "number");
+			x.value = 0;
+				//x.innerHTML = job.job_output_vector[i] - job.job_upkeep_vector[i] - job.pop_upkeep_vector[i];
+			c.appendChild(x);
+		}
+
+		document.body.insertBefore(t, document.getElementById("targets_insertion_point"));
+	}
+
+	create_jobs_needed_table() {
+		var c, r, t;
+		t = this.jobs_needed_table;
+		t.width = 400;
+		r = t.insertRow(); 
+		c = r.insertCell();
+		for (let job of this.om.jobs_list) {
+			c = r.insertCell();
+			c.innerHTML = "<img src= " + job.symbol_url + " alt=\"Job_Symb\" width=\"20\" height=\"20\"> ";
+		}
+		r = t.insertRow();
+		c = r.insertCell();
+		c.innerHTML = "Jobs Needed: ";
+		for (let i in this.om.resources_list) {
+			c = r.insertCell();
+			c.innerHTML = 0;	
+			
+		}
+
+		document.body.insertBefore(t, document.getElementById("jobs_needed_insertion_point"));
 
 	}
 
 
-	get_target() {
+	calculate() {
 		let target = [0,0,0,0,0,0];
-		target[0] = document.getElementById('Ec_Target').value;
-		target[1] = document.getElementById('Min_Target').value;
-		target[2] = document.getElementById('Food_Target').value;
-		target[3] = document.getElementById('Alloys_Target').value;
-		target[4] = document.getElementById('Cg_Target').value;
-		target[5] = document.getElementById('Science_Target').value;
-		
+		var t = this.target_table;
+			
+			let row = t.rows[1];
+				for (let j in row.cells) {
+					if (j > 0) {
+						let cell = row.cells[j];
+						let val = cell.children[0].value;
+						target[j-1] = parseFloat(val);
+					
+					}
+				}  
 		this.om.set_target(target);
 		this.om.attain_target();
-		document.getElementById("jobs_needed").innerHTML = this.om.jobs_needed;
-;		
+		//document.getElementById("jobs_needed").innerHTML = this.om.jobs_needed;
+
+		t = this.jobs_needed_table;
+		row = t.rows[1];
+		for (let j in row.cells) {
+			if (j > 0) {
+				let cell = row.cells[j];
+				cell.innerHTML = this.om.jobs_needed[j-1];
+			}
+		}
+		let x = 0;
+		for (let i of this.om.jobs_needed) {
+			x += parseFloat(i);
+		}
+;		document.getElementById("total_jobs").innerHTML = "total jobs needed: " + String(x.toFixed(1));
 	}
 
 	set_jobs_table() {
@@ -71,21 +167,29 @@ class page_model {
 		t.width = 400;
 		r = t.insertRow(); 
 		c = r.insertCell();
-		for (let resource of this.resource_List) {
+		for (let resource of this.om.resources_list) {
 			c = r.insertCell();
 			c.innerHTML = "<img src= " + resource.symbol_url + " alt=\"Resource_Symb\" width=\"20\" height=\"20\"> ";
 		}
 		for (let job of this.om.jobs_list) {
 			r = t.insertRow();
 			c = r.insertCell();
-			c.innerHTML = "<img src= " + job.job_sybmol_url + " alt=\"Job_Symb\" width=\"20\" height=\"20\"> ";
+			c.innerHTML = "<img src= " + job.symbol_url + " alt=\"Job_Symb\" width=\"20\" height=\"20\"> ";
 			for ( let i in job.job_output_vector) {
 				c = r.insertCell();
 				c.innerHTML = job.job_vector[i];
-				c.size = 1;
-				//var x = document.createElement("INPUT");
-				//x.setAttribute("type", "text");
-				//c.appendChild(x);
+				c.style.fontWeight="bold";
+				if (job.job_vector[i]>0) {
+					c.style.color = "green";
+				}
+				if (job.job_vector[i] < 0) {
+					c.style.color = "red";
+				}
+				
+				if (job.job_vector[i] == 0) {
+					c.style.color = "orange";
+				}
+				
 			}
 		}	
 		var insertion_point = document.getElementById("table_insertion_point");
@@ -106,14 +210,14 @@ class page_model {
 		t.width = 400;
 		r = t.insertRow(); 
 		c = r.insertCell();
-		for (let resource of this.resource_List) {
+		for (let resource of this.om.resources_list) {
 			c = r.insertCell();
 			c.innerHTML = "<img src= " + resource.symbol_url + " alt=\"Resource_Symb\" width=\"20\" height=\"20\"> ";
 		}
 		for (let job of this.om.jobs_list) {
 			r = t.insertRow();
 			c = r.insertCell();
-			c.innerHTML = "<img src= " + job.job_sybmol_url + " alt=\"Job_Symb\" width=\"20\" height=\"20\"> ";
+			c.innerHTML = "<img src= " + job.symbol_url + " alt=\"Job_Symb\" width=\"20\" height=\"20\"> ";
 			for ( let i in job.job_output_vector) {
 				c = r.insertCell();
 				
@@ -166,18 +270,10 @@ class page_model {
 	document.body.insertBefore(edit_button, document.getElementById("apply_button"));
 	document.getElementById("apply_button").remove();
 	document.getElementById("edit_jm_button").addEventListener("click", this.boundEdit);
-	
-	
 	}
-
 }
 
 let pm = new page_model();
-//pm.set_jobs_table();
-//pm.edit_jobs_table();
 
-document.getElementById("calculate_button").addEventListener("click", pm.boundGet);
+document.getElementById("calculate_button").addEventListener("click", pm.boundCalculate);
 document.getElementById("edit_jm_button").addEventListener("click", pm.boundEdit);
-
-document.getElementById("jobs").innerHTML = "Technicians, Miners, Farmers, Metallurgists, Artisans, Researchers";
-document.getElementById("resources").innerHTML = pm.om.resource_order
